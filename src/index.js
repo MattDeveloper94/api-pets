5c3b5164dcc47992d079b43e7940e8103b34df56
Matheus Santana Rodrigues <matheusrodrigues94@icloud.com>
2026-01-28 02:02:10 -0300

    Rotas obter pelo ID, atualizar e deletar um pet inseridas

--- src/index.js
+++ src/index.js
@@ -64,6 +64,106 @@ app.post("/pets", (req, res) => {
     }
 });
 
+//Obter um pet pelo seu ID
+// GET → /pets/:id
+app.get("/pets/:id", (req, res) => {
+    try {
+        // entrada
+        const { id } = req.params;
+
+        // processamento
+        const pet = pets.find(item => item.id === id)
+        if (!pet) {
+            return res.status(404).send({
+                ok: false,
+                mensagem: "Pet não encontrado, verifique o ID inserido."
+            });
+        }
+        // saída
+        res.status(200).send({
+            ok: true,
+            mensagem: "Pet obtido com sucesso!",
+            dados: pet
+        });
+
+
+    } catch (error) {
+        res.status(500).send({
+            ok: false,
+            mensagem: error.toString()
+        });
+    }
+});
+
+//Atualizar um pet pelo seu ID
+// PUT → /pets/:id
+app.put("/pets/:id", (req, res) => {
+    try {
+        //entrada
+        const { id } = req.params;
+        const { nome, idade, raca, nomeTutor } = req.body
+
+        //processamento
+        const pet = pets.find(item => item.id === id);
+        if (!pet) {
+            return res.status(404).send({
+                ok: false,
+                mensagem: "Pet não encontrado, verifique o ID inserido."
+            });
+        }
+
+        pet.nome = nome;
+        pet.idade = idade;
+        pet.raca = raca;
+        pet.nomeTutor = nomeTutor;
+
+        //saída
+        res.status(200).send({
+            ok: true,
+            mensagem: "Pet atualizado com sucesso!",
+            dados: pets
+        });
+    } catch (error) {
+        res.status(500).send({
+            ok: false,
+            mensagem: error.toString()
+        });
+    }
+});
+
+//Deletar pet pelo seu ID
+// DELETE → /pets/:id
+app.delete("/pets/:id", (req, res) => {
+    try {
+        //entrada
+        const { id } = req.params;
+
+        //processamento
+        const petIndex = pets.findIndex(item => item.id === id);
+        if (petIndex < 0) {
+            return res.status(404).send({
+                ok: false,
+                mensagem: "Pet não encontrado, verifique o ID inserido."
+            });
+        }
+
+        pets.splice(petIndex, 1);
+
+        //saída
+        res.status(200).send({
+            ok: true,
+            mensagem: "Pet deletado com sucesso!",
+            dados: pets
+        });
+
+    } catch (error) {
+        res.status(500).send({
+            ok: false,
+            mensagem: error.toString()
+        });
+    }
+});
+
 
 // executando API
 const porta = process.env.PORT;
